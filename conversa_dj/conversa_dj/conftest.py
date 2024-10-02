c7e4dad223de724acca5acdd785d1693347ee45e
davidnjoroge1 <ndegwa.david@outlook.com>
2024-10-02 10:34:38 +0300

    project initialization and dependencies

---
+++ conversa_dj/conversa_dj/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from conversa_dj.users.models import User
+from conversa_dj.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
